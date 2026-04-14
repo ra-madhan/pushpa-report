7cd09d9fc36c900072bb3239d907cec832e46f18
Madhan <ramadhan062007@gmail.com>
2026-04-14 22:04:56 +0530

    NOTHING

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA52MaRGQ2m1iAX21zbdKMvweEqsPoZL3w",
+  authDomain: "pushpa-report.firebaseapp.com",
+  projectId: "pushpa-report",
+  storageBucket: "pushpa-report.firebasestorage.app",
+  messagingSenderId: "209660404105",
+  appId: "1:209660404105:web:74fa46b3f860c3ec508158"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
